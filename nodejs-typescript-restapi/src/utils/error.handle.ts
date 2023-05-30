20f9fd0551a428c8839d4e0228de7142e42b7f23
Matias Gonzalez <mng_epson@hotmail.com>
2023-05-30 07:42:04 -0300

    [feat: add items controller and handleHttp]

---
+++ nodejs-typescript-restapi/src/utils/error.handle.ts
@@ -0,0 +1,10 @@
+//función para manejar mensajes de errores
+
+import { Response } from "express"
+
+const handleHttp = (res:Response, error:string)=>{
+  res.status(500)
+  res.send({error})
+}
+
+export {handleHttp};
